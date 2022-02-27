651b3e781d2720040d6a1cb4c384fcaa312be0e0
rokib97 <hasanrokib1997@gmail.com>
2022-02-27 22:19:07 +0600

    added

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const loadData = () => {
   const inputField = document.getElementById("input-field");
   document.getElementById("error-msg").innerHTML = "";
   document.getElementById("details").innerHTML = "";
+  document.getElementById("spinner").style.display = "block";
   const inputValue = inputField.value;
   const errorMsg = document.getElementById("error-msg");
   if (!isNaN(inputValue) || inputValue == "") {
@@ -25,8 +26,10 @@ const loadData = () => {
           Please input valid something....
           </marquee>
           `;
+          document.getElementById("card-container").innerHTML = "";
         } else {
           displayData(data.drinks);
+          document.getElementById("spinner").style.display = "none";
         }
       });
     inputField.value = "";
@@ -44,7 +47,7 @@ const displayData = (drinks) => {
 
     div.innerHTML = `
         <div class="col">
-          <div class="card h-100 shadow-lg rounded-3">
+          <div class="card h-100 shadow-lg p-3">
             <img src="${drink.strDrinkThumb}" class="card-img-top" alt="..." />
             <div class="card-body">
               <h5 class="card-title">${drink.strDrink}</h5>
@@ -58,18 +61,20 @@ const displayData = (drinks) => {
 };
 
 const loadDetails = (id) => {
+  document.getElementById("spinner").style.display = "block";
   // console.log(id);
   const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => displayDetails(data.drinks[0]));
+  document.getElementById("spinner").style.display = "none";
 };
 
 const displayDetails = (drink) => {
   console.log(drink);
   const detailsMainDiv = document.getElementById("details");
   const div = document.createElement("div");
-  div.classList.add("card", "shadow-lg");
+  div.classList.add("card", "shadow-lg", "p-4");
   document.getElementById("details").innerHTML = "";
   div.innerHTML = `
       <img src="${drink.strDrinkThumb}" class="card-img-top img-fluid " alt="..." />
